8876077fe85e7da1549ccb176b7db26efc069349
Patrick Wang <patrick.wang@chem.ox.ac.uk>
2025-06-11 10:39:23 +0100

    updated conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../../'))
 
 print("PYTHONPATH for Sphinx build:", sys.path)
 # -- Project information -----------------------------------------------------
